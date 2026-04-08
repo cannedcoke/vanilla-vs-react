edf30d2ed3f504db22a1f76f7833c3a8cbdc0336
Cyanide <miqueas736d@gmail.com>
2026-04-08 19:26:21 -0300

    add: adding shema for tags

--- models/recordSchema.js
+++ models/recordSchema.js
@@ -1,17 +1,27 @@
-const mongoose = require("mongoose")
-// esquema para los recordos
-const recordSchema = new mongoose.Schema({
-    tema:{
-        type:String,
-        required:true,
-        unique:true
+const mongoose = require("mongoose");
+
+const recordSchema = new mongoose.Schema(
+  {
+    tema: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    comment:{
-        type:String,
-        default:"",
-        maxlenght:500
+    comment: [
+      {
+        type: String,
+        maxlength: 500,
+      },
+    ],
+    etiquetas: [{ type: mongoose.Schema.Types.ObjectId, ref: "Tag" }],
+    votes: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    collection: "Record",
+  },
+);
 
-    }
-});
-
-module.exports = mongoose.model("Record",recordSchema)
+module.exports = mongoose.model("Record", recordSchema);
---
+++ models/tagSchema.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const tagSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, unique: true },
+  },
+  { collection: "Tag" },
+);
+
+module.exports = mongoose.model("Tag", tagSchema);
