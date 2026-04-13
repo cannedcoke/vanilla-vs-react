a00ac74d8a63b73eee094a1764da904547a97111
Cyanide <miqueas736d@gmail.com>
2026-04-13 13:34:34 -0300

    add: adding comments

--- models/tagSchema.js
+++ models/tagSchema.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+// schema of the tag collection
 const tagSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, unique: true },
--- routers/router.js
+++ routers/router.js
@@ -2,12 +2,15 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/controller")
 
-router.post("/api/filter",controller.filter );
+// rotas para acciones de usuario
 router.post("/api/addLink",controller.addLink );
 router.post("/api/addTag",controller.addTag );
-router.post("/api/details",controller.details );
 router.post("/api/addVote",controller.addVote );
-router.get("/api/pupulateSelect",controller.pupulateSelect );
 router.post("/api/addComment",controller.addComment );
+
+// rutas para acciones del front
+router.post("/api/details",controller.details );
+router.post("/api/filter",controller.filter );
+router.get("/api/populateSelect",controller.populateSelect );
 
 module.exports = router;
